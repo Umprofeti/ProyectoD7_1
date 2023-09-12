e03d5dbcde39676502d4dc0028eb5483ee62533e
David R. Lee <davidricardolee@gmail.com>
2023-09-12 12:45:55 -0500

    Aguste en el tamaño del form, fondo y letra

--- index.php
+++ index.php
@@ -1,182 +1,247 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="./jquery-3.7.1.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
     <title>Proyecto #1 Desarrollo 7</title>
 </head>
-    <style>
-        *{
-            margin: 0;
-            padding: 0;
-         }
-         section{
-            position:relative;
-            width:100%;
-            height:100vh;
-            background:#3586ff;
-            overflow: hidden;
-         }
-         section.wave{
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width:100%;
-            height: 100px;
-            background: url('wave.png');
-            background-size: 1000px 100px;
-         }
-         section.wave.wave1{
-            animation: animate 30s linear infinite;
-            z-index: 1000;
-            opacity: 1;
-            animation-delay: 0s;
-            bottom: 0;
-         }
-         section.wave.wave2{
-            animation: animate2 15s linear infinite;
-            z-index: 999;
-            opacity: 0.5;
-            animation-delay: -5s;
-            bottom: 10px;
-         }
-         section.wave.wave3{
-            animation: animate 30s linear infinite;
-            z-index: 998;
-            opacity: 0.2;
-            animation-delay: -2s;
-            bottom: 15px;
-         }
-         section.wave.wave4{
-            animation: animate2 5s linear infinite;
-            z-index: 997;
-            opacity: 0.7;
-            animation-delay: -5s;
-            bottom: 20px;
-         }
-        @keyframes animate{
-            0%{
-                background-position-x: 0;
-            }
-            100%{
-                background-position-x: 1000px;
-            }
-        }
-        @keyframes animate2{
-            0%{
-                background-position-x: 0;
-            }
-            100%{
-                background-position-x: -1000px;
-            }
-        }
+<style>
+    main {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        gap: 30px;
+    }
+
+    form.formularioContainer {
+        background-color: white;
+        margin: 5% auto;
+        display: flex;
+        width: 40%;
+        justify-content: center;
+        padding: 10px 20px 20px 10px;
+        align-items: start;
+        flex-direction: column;
+        gap: 30px;
+        border: none;
+        border-radius: 10px;
+        box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.10);
+        color: #000;
+    }
+
+    div.fRow {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        text-align: left;
+        font-family: 'Lato', sans-serif;
+        gap: 5px;
+        width: 100%;
+    }
+
+    input {
+        width: 90%;
+        border: 0;
+        box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.10);
+        border-radius: 15px;
+        padding: 15px;
+    }
+
+    button {
+        margin: 0 auto;
+        box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.10);
+        padding: 10px;
+        border: none;
+        background-color: #fff;
+        color: #black;
+        font-weight: 600;
+        border-radius: 5px;
+        width: 20%;
+        font-family: 'Lato', sans-serif;
+    }
+
+    div.msg.MensajeError {
+        border: solid 3px #FF0060;
+        padding: 5px 10px 10px 5px;
+        font-family: 'Lato', sans-serif;
+        background: rgba(255, 87, 51, 0.2);
+    }
 
 
-        main {
-            display: flex;
-            width: 100%;
-            height: 100%;
-            justify-content: center;
-            flex-direction: column;
-            align-items: center;
-            gap: 30px;
+
+
+    body {
+        margin: 0;
+    }
+
+
+
+    .header {
+        position: relative;
+        text-align: center;
+        background: -o-linear-gradient(30deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%);
+        background: linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%);
+        color: #fff;
+    }
+
+    .inner-header {
+        height: 20vh;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    .content {
+        position: relative;
+        height: 20vh;
+        text-align: center;
+        background-color: #fff;
+    }
+
+    .flex {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        text-align: center;
+    }
+
+    .waves {
+        position: relative;
+        width: 100%;
+        height: 15vh;
+        margin-bottom: -7px;
+        min-height: 100px;
+        max-height: 100px;
+    }
+
+    .parallax>use {
+        -webkit-animation: moveForever 25s cubic-bezier(.55, .5, .45, .5) infinite;
+        animation: moveForever 25s cubic-bezier(.55, .5, .45, .5) infinite;
+    }
+
+    .parallax>use:nth-child(1) {
+        -webkit-animation-delay: -2s;
+        animation-delay: -2s;
+        -webkit-animation-duration: 7s;
+        animation-duration: 7s;
+    }
+
+    .parallax>use:nth-child(2) {
+        -webkit-animation-delay: -3s;
+        animation-delay: -3s;
+        -webkit-animation-duration: 10s;
+        animation-duration: 10s;
+    }
+
+    .parallax>use:nth-child(3) {
+        -webkit-animation-delay: -4s;
+        animation-delay: -4s;
+        -webkit-animation-duration: 13s;
+        animation-duration: 13s;
+    }
+
+    .parallax>use:nth-child(4) {
+        -webkit-animation-delay: -5s;
+        animation-delay: -5s;
+        -webkit-animation-duration: 20s;
+        animation-duration: 20s;
+    }
+
+    @-webkit-keyframes moveForever {
+        0% {
+            -webkit-transform: translate3d(-90px, 0, 0);
+            transform: translate3d(-90px, 0, 0);
         }
-        form.formularioContainer{
-            background-color: white;
-            margin: 5% auto; 
-            display: flex;
-            justify-content: center;
-            padding: 10px 20px 20px 10px;
-            align-items: start;
-            flex-direction: column;
-            gap:30px;
-            border:none;
-            border-radius:10px;  
-            box-shadow:4px 4px 10px rgba(0,0,0,0.10);
+
+        100% {
+            -webkit-transform: translate3d(85px, 0, 0);
+            transform: translate3d(85px, 0, 0);
         }
-       
-        div.fRow{
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            text-align: left;
-            font-family: Arial, Helvetica, sans-serif;
-            gap: 5px;
+    }
+
+    @keyframes moveForever {
+        0% {
+            -webkit-transform: translate3d(-90px, 0, 0);
+            transform: translate3d(-90px, 0, 0);
         }
-        input{
-            width: 90%;
-            border:0;
-            box-shadow:4px 4px 10px rgba(0,0,0,0.10);
-            border-radius:15px;
-            padding:15px;
+
+        100% {
+            -webkit-transform: translate3d(85px, 0, 0);
+            transform: translate3d(85px, 0, 0);
         }
-        button{
-            margin:0 auto;
-            box-shadow:4px 4px 10px rgba(0,0,0,0.10);
-            padding:10px;
-            border:none;
-            background-color:#fff;
-            color:#black;
-            font-weight:600;
-            border-radius:5px;
-            width:50%;
-         }
-       
-        div.msg.MensajeError{
-            border: solid 3px #FF0060;
-            padding: 5px 10px 10px 5px;
-            font-family: Helvetica, san-serif;
-            background: rgba(255, 87, 51, 0.2);
+    }
+
+    @media (max-width : 768px) {
+        h1 {
+            font-size: 24px;
         }
-        </style> 
+
+        .content {
+            height: 30vh;
+        }
+
+        .waves {
+            height: 40px;
+            min-height: 40px;
+        }
+    }
+</style>
+
 <body>
-    <main>
-        <form class="formularioContainer" name="Cheque" id="formCheque">
-            <div class="fRow">
-                <label for="Ncheqe">Nºcheque</label>
-                <input type="text" name="NCheque" id="NCheque" placeholder="Número de Cheque">
-                <label for="Fecha">fecha:</label>
-                <input type="date" name="Fecha" id="Fecha" placeholder="Fecha">
-            </div>
+    <div class="header">
+        <div class="inner-header flex">
+        </div>
+        <main>
+            <form class="formularioContainer" name="Cheque" id="formCheque">
+                <div class="fRow">
+                    <label for="Ncheqe">Nºcheque</label>
+                    <input type="text" name="NCheque" id="NCheque">
+                    <label for="Fecha">fecha:</label>
+                    <input type="date" name="Fecha" id="Fecha">
+                </div>
 
-            <div class="fRow">
-            <label for="Nombre">Nombre:</label>
-            <input type="text" name="Nombre" id="Nombre" placeholder="Nombre">
-            </div>
+                <div class="fRow">
+                    <label for="Nombre">Nombre:</label>
+                    <input type="text" name="Nombre" id="Nombre" placeholder="Nombre">
+                </div>
 
-            <div class="fRow">
-                <label for="Cant">Cantidad:</label>
-                <input type="text" name="Cant" id="Cant" placeholder="Cantidad">
-                <input type="text" name="CantString" id="CantString" disabled>
-            </div>
+                <div class="fRow">
+                    <label for="Cant">Cantidad:</label>
+                    <input type="text" name="Cant" id="Cant" placeholder="Cantidad">
+                    <input type="text" name="CantString" id="CantString" disabled>
+                </div>
 
-            <div class="fRow">
-            <label for="DGasto">Descripción:</label>
-            <input type="text" name="DGasto" id="DGasto"
-            placeholder="Descripción del gasto">
-            </div>
+                <div class="fRow">
+                    <label for="DGasto">Descripción:</label>
+                    <input type="text" name="DGasto" id="DGasto" placeholder="Descripción del gasto">
+                </div>
 
-            <button value="Enviar" onclick="sendForm()">Enviar</button>
-        </form>
-        <div class="msg "id="msg"></div>
-        <div class="msg "id="msgPHP"></div>
+                <button value="Enviar" onclick="sendForm()">Enviar</button>
+            </form>
+            <div class="msg " id="msg"></div>
+            <div class="msg " id="msgPHP"></div>
 
-    <section>
-        <div class="wave wave1"></div>
-        <div class="wave wave2"></div>
-        <div class="wave wave3"></div>
-        <div class="wave wave4"></div>
-    </section>
-
-
-    </main>
-    <script src="./index.js"></script>
-    <script>
-        $("form").on("submit",(e)=>{
-            e.preventDefault();
-        })
-        const sendForm = ()=> {
+        </main>
+        <script src="./index.js"></script>
+        <script>
+            $("form").on("submit", (e) => {
+                e.preventDefault();
+            })
+            const sendForm = () => {
                 $.ajax({
                     type: "POST",
                     url: "validacion.php",
@@ -186,6 +251,20 @@
                     }
                 })
             }
-    </script>
+        </script>
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="parallax">
+                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+        </svg>
+    </div>
 </body>
+
 </html>
